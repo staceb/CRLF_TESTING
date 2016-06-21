4c6e4eb1f17732cefcf305b905e28d2b67c632ab
Stacey Burns <staceyburnsy@gmail.com>
2016-06-21 12:24:40 +0200

    mirrir

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var test = function(){
   var something = 'something';
 }
-
-// testing 2 
+vvv
+// testing 2
 var test2 = function(){
 
   var something2 = 'something2';
 }
 
 var test 3 = function(){
-  
+
   var something3 = 'something 3';
 }
